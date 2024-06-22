7c49af66584b78b7e93be8b9c9fd16ad9f48e007
Soham Pathak <soham.pathak@datenwissen.com>
2024-06-22 22:07:58 +0530

    Pushed the footer detailing

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         secondary: "#A259FF",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
+        textGrey:"#CCCCCC",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
