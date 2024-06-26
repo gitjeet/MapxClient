2423e949a900c5b58f8d037b8dcb345b9d4a9915
Soham Pathak <soham.pathak@datenwissen.com>
2024-06-26 20:59:13 +0530

    Created the page for the nft specific

--- src/pages/NFTProfile/index.jsx
+++ src/pages/NFTProfile/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../../style";
+import { User, logo } from "../../assets/index";
+import MoreArtistNFT from "./MoreArtistNFT";
 
 const NFTProfile = () => {
   return (
@@ -11,20 +13,30 @@ const NFTProfile = () => {
             src="https://s3-alpha-sig.figma.com/img/a6f4/3f93/50c73503d13e1fe1a0e3277804ef1192?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WfwG7Ha3EPdBnGfQnV22T2QDnea~WD~mrP--H6kCvJvnQMT2KCphxXckercVaZl8D~dxLqltUfvRrtHiYllAFaEqGcP84y0FPoijsY3N8urtG7NrM6Ra~J3Tfux0L61OVCgRYERYg9chEqJxkgwV7ly4e1nPdNjucVSKYd-zVmEfiTZf8i9U003mS22oqitcBP4DfR18puA8gUat8U51R~WjjBElOvr6eSVAhcm3aPCxOPmqdXSNHoEqVfwFyvOYrs-ywsgZ3vGedK-~0IbBxWT3LVYO3LujjdfTa1oyMs9lC4qEKW8OJi6jM~7725sqI4hmy8fYtJdJN3hn6DAWiA__"
           />
         </div>
-        <div className="flex flex-1 flex-col justify-between sm:flex-row mt-10 mx-24">
+        <div className="flex flex-1 flex-col justify-between sm:flex-row mt-10 md:mx-24">
           <div className="flex flex-1 flex-col max-w-[605px]">
-            <h2 className="text-[28px] sm:text-[38px] md:text-[51px] text-white font-semibold">The Orbidians</h2>
-            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-thin">Minted on Sep 30,2022</h2>
-            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">Created by</h2>
+            <h2 className="text-[28px] sm:text-[38px] md:text-[51px] text-white font-semibold">
+              The Orbidians
+            </h2>
+            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-thin">
+              Minted on Sep 30,2022
+            </h2>
+            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">
+              Created by
+            </h2>
             <div className="flex items-center mt-2">
               <img
                 src="/mnt/data/image.png"
                 alt="Author"
                 className="w-6 h-6 rounded-full mr-2"
               />
-              <span className="text-white font-semibold text-[16px] md:text-[22px]">Animakid</span>
+              <span className="text-white font-semibold text-[16px] md:text-[22px]">
+                Animakid
+              </span>
             </div>
-            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">Description</h2>
+            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">
+              Description
+            </h2>
             <p className="text-[16px] md:text-[22px] text-white">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
               autem sequi tempora eos, temporibus quibusdam! Assumenda harum
@@ -44,24 +56,34 @@ const NFTProfile = () => {
               officia? Quaerat neque eaque commodi molestiae atque ad veritatis,
               consequuntur repellat officia distinctio.
             </p>
-            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">Details</h2>
-            
-            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">Tags</h2>
-            <div className="flex flex-wrap ">
-              <h2 className="py-2 text-white font-semibold bg-lightGrey rounded-2xl px-2">
+            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">
+              Details
+            </h2>
+            <div className="flex">
+              <img src={User} />
+              <h2 className="text-white text-[16px] sm:text-[22px] ml-3">View on Etherscan</h2>
+            </div>
+            <div className="flex">
+              <img src={User} />
+              <h2 className="text-white text-[16px] sm:text-[22px] ml-3">View Original</h2>
+            </div>
+            <h2 className="text-[16px] md:text-[22px] text-dimWhite font-semibold">
+              Tags
+            </h2>
+            <div className="flex flex-col w-full md:flex-row justify-between">
+              <h2 className="py-2 mx-1 my-1 w-fit text-white font-semibold bg-lightGrey rounded-2xl px-10">
                 ANIMATION
               </h2>
-              <h2 className="py-2 text-white font-semibold bg-lightGrey rounded-2xl px-2">
-                ANIMATION
+              <h2 className="py-2 mx-1 my-1 w-fit text-white font-semibold bg-lightGrey rounded-2xl px-10">
+                ILLUSTRATION
               </h2>
-              <h2 className="py-2 text-white font-semibold bg-lightGrey rounded-2xl px-2">
-                ANIMATION
+              <h2 className="py-2 mx-1 my-1 w-fit text-white font-semibold bg-lightGrey rounded-2xl px-10">
+                MOON
               </h2>
-              <h2 className="py-2 text-white font-semibold bg-lightGrey rounded-2xl px-2">
-                ANIMATION
+              <h2 className="py-2 mx-1 my-1 w-fit text-white font-semibold bg-lightGrey rounded-2xl px-10">
+                MOON
               </h2>
             </div>
-            
           </div>
           <div className="w-full flex-col sm:w-[295px] p-4 h-[234px] bg-lightGrey rounded-xl">
             <h2 className="text-dimWhite font-light">Auction ends in </h2>
@@ -86,6 +108,7 @@ const NFTProfile = () => {
             </div>
           </div>
         </div>
+        <MoreArtistNFT/>
       </div>
     </div>
   );
